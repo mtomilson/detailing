53976fd44ccf1f0df82250dfc74d236ab419a5d7
Kent Dang <kent_2015@outlook.com>
2024-08-26 23:15:11 -0400

    Underline items on hover for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,13 @@ import {Nav, Navbar} from 'react-bootstrap'
 export default function NavBar() {
   return (
     <Navbar className='bg-white sticky top-0 py-4 flex items-center justify-between px-4 shadow-[0_1px_rgba(0,0,0,0.075)]'>
+      {/* TODO: Add the logo and hove affect */}
       <h1 className='flex space-x-8 font-medium'>Logo Here</h1>
-      <ul className='flex space-x-8 font-medium'>
-      <Nav.Link href='#hero'>Home</Nav.Link>
-      <Nav.Link href='#services'>Services</Nav.Link>
-      <Nav.Link href='#gallery'>Gallery</Nav.Link>
-      <Nav.Link href='#contact'>Contact</Nav.Link>
+      <ul className='flex space-x-8 font-medium '>
+      <Nav.Link href='#hero' className='hover:underline decoration-solid'>Home</Nav.Link>
+      <Nav.Link href='#services' className='hover:underline decoration-solid'>Services</Nav.Link>
+      <Nav.Link href='#gallery' className='hover:underline decoration-solid'>Gallery</Nav.Link>
+      <Nav.Link href='#contact' className='hover:underline decoration-solid'>Contact</Nav.Link>
       </ul>
       
     </Navbar>
