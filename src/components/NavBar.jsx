5964adb3ccd0dad209224519683db4a86fe6fcd4
Kent Dang <kent_2015@outlook.com>
2024-08-29 20:01:44 -0400

    Divided the header into two sections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,13 +22,13 @@ export default function NavBar() {
     <Navbar
       className={
         scrolled
-          ? "bg-white sticky top-0 py-4 flex items-center justify-between px-4 shadow-[0_1px_rgba(0,0,0,0.075)]"
-          : "'bg-transparent sticky top-0 py-4 flex items-center justify-between px-4 shadow-[0_1px_rgba(0,0,0,0.075)]"
+          ? "bg-white fixed w-full top-0 py-4 flex items-center justify-between px-4 shadow-[0_1px_rgba(0,0,0,0.075)]"
+          : "bg-transparent fixed w-full top-0 py-4 flex items-center justify-between px-4"
       }
     >
       {/* TODO: Add the logo and hove affect */}
       <h1 className="flex space-x-8 font-medium">Logo Here</h1>
-      <ul className="flex space-x-8 font-medium ">
+      <ul className="flex space-x-8 font-medium">
         <Nav.Link href="#hero" className="hover:underline decoration-solid">
           Home
         </Nav.Link>
